73f73c3c9ab092b7525816220b6786ec2963a20b
lzlniu <lzlniu@qq.com>
2020-02-13 14:05:20 +0800

    Update call_variant.sh

--- call_variant.sh
+++ call_variant.sh
@@ -2,7 +2,8 @@
 #author:Zelin Li
 #date:2020.02.13
 #utility:search for snp and indel from paired-end clean data(reads,fastq format)
-cd /public/home/lizelin/data/Pg/Pgcleandata
+bwa index /PATH/TO/BWA/reference/sequence/ref.fa
+cd /PATH/TO/WHERE/YOU/WANT/TO/GET/RESULT
 for i in $(cat list);do
 echo "#BSUB -L /bin/bash
 #BSUB -J run_${i}.sh
@@ -13,13 +14,13 @@ echo "#BSUB -L /bin/bash
 
 mkdir ${i}
 cd ${i}
-bwa mem /public/home/lizelin/data/Pg/Pgcleandata/CNS00067.fa /public/home/lizelin/data/Pg/Pgcleandata/tar/${i}_1_clean.fq /public/home/lizelin/data/Pg/Pgcleandata/tar/${i}_2_clean.fq -o aln.sam
+bwa mem /PATH/TO/BWA/reference/sequence/ref.fa /PATH/TO/sequencing/clean/data/${i}_1_clean.fq /PATH/TO/sequencing/clean/data/${i}_2_clean.fq -o aln.sam
 samtools view -bS -F 12 aln.sam -o aln.bam
 samtools sort aln.bam -o aln_sort.bam
 samtools index aln_sort.bam
-samtools mpileup -f /public/home/lizelin/data/Pg/Pgcleandata/CNS00067.fa aln_sort.bam -o aln.mpileup
-java -jar /public/home/lizelin/align/VarScan.v2.4.4.jar mpileup2cns aln.mpileup --output-vcf 1 --variants 1 > all.vcf
-java -jar /public/home/lizelin/align/VarScan.v2.4.4.jar mpileup2snp aln.mpileup --output-vcf 1 > snp.vcf
+samtools mpileup -f /PATH/TO/BWA/reference/sequence/ref.fa aln_sort.bam -o aln.mpileup
+java -jar VarScan.v2.4.4.jar mpileup2cns aln.mpileup --output-vcf 1 --variants 1 > all.vcf
+java -jar VarScan.v2.4.4.jar mpileup2snp aln.mpileup --output-vcf 1 > snp.vcf
 grep -n "HOM=1" snp.vcf > snv_${i}.txt
 rm -rf aln.sam
 cp snv_${i}.txt ../
