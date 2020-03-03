ece2552bfa97ca16cc5804d4a61c71f5f3d9434d
lzlniu <lzlniu@qq.com>
2020-03-03 16:20:17 +0800

    Update gbaa.sh

--- gbaa.sh
+++ gbaa.sh
@@ -2,7 +2,7 @@
 #author:lizelin
 #date:2020/03/03
 #utility:extract the amino acids sequence from the .gb file.
-#use like:gbaa.sh sequence.gb (this will generate a file call protseqs.fa which contain amino acids sequence of each CDS from this .gb file)
+#use like: gbaa.sh input.gb > output (this will generate an output which contain amino acids sequence of each CDS from this .gb file)
 gbfile=$1
 sed -e '/\/translation=\"/,/\"/!d' $gbfile | sed -e '/  gene  /,/\/gene=\".*\"/d' | sed -e 's/ //g' | sed -e 's/\//\n/g' | sed -e 's/translation="/>\n/g' | sed -e 's/\"//g' | awk '!/^>/ { printf "%s", $0; n = "\n" } /^>/ { print n $0; n = "" } END { printf "%s", n }' | sed -e '1d' > protseqs.fa
 grep -n ">" protseqs.fa | awk -F ':' '{print $1}' > protnamelines
@@ -16,3 +16,5 @@ done
 rm -rf protname
 rm -rf protnames
 rm -rf protnamelines
+cat protseqs.fa
+rm -rf protseqs.fa
