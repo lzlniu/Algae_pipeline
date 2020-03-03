60890aeb6e4a1113f5f1a59ff5db82eea8fc5a18
lzlniu <lzlniu@qq.com>
2020-03-03 15:26:25 +0800

    Update gbaa.sh

--- gbaa.sh
+++ gbaa.sh
@@ -4,9 +4,9 @@
 #utility:extract the amino acids sequence from the .gb file.
 #use like:gbaa.sh sequence.gb (this will generate a file call protseqs.fa which contain amino acids sequence of each CDS from this .gb file)
 gbfile=$1
-cat $gbfile | sed -e '/\/translation=\"/,/\"/!d' sequence.gb | sed -e '/  gene  /,/\/gene=\".*\"/d' | sed -e 's/ //g' | sed -e 's/\//\n/g' | sed -e 's/translation="/>\n/g' | sed -e 's/\"//g' | awk '!/^>/ { printf "%s", $0; n = "\n" } /^>/ { print n $0; n = "" } END { printf "%s", n }' | sed -e '1d' > protseqs.fa
+sed -e '/\/translation=\"/,/\"/!d' $gbfile | sed -e '/  gene  /,/\/gene=\".*\"/d' | sed -e 's/ //g' | sed -e 's/\//\n/g' | sed -e 's/translation="/>\n/g' | sed -e 's/\"//g' | awk '!/^>/ { printf "%s", $0; n = "\n" } /^>/ { print n $0; n = "" } END { printf "%s", n }' | sed -e '1d' > protseqs.fa
 grep -n ">" protseqs.fa | awk -F ':' '{print $1}' > protnamelines
-sed -e '/  CDS  /,/\/gene=\".*\"/!d' sequence.gb | grep "/gene=" | sed -e 's/.*\/gene=//g' | sed -e 's/\"//g' > protnames
+sed -e '/  CDS  /,/\/gene=\".*\"/!d' $gbfile | grep "/gene=" | sed -e 's/.*\/gene=//g' | sed -e 's/\"//g' > protnames
 k=1
 for protnameline in $(cat protnamelines); do
 	sed -n "$k"p protnames > protname
