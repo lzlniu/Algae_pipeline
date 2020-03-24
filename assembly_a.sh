4d860be4f6f45e72f1a5b7c2ec39af775e8b97f3
lzlniu <lzlniu@qq.com>
2020-03-25 02:17:32 +0800

    Update assembly_a.sh

--- assembly_a.sh
+++ assembly_a.sh
@@ -7,14 +7,14 @@ readsfq_path=$1
 for i in $(cat list); do
 echo "#BSUB -L /bin/bash
 #BSUB -J abyss_${i}.sh
-#BSUB -q fat
 #BSUB -n 4
 #BSUB -o abyss_${i}.out
 #BSUB -e abyss_${i}.err
 
 cd ${i}
-mkdir abyss && cd abyss
-abyss-pe np=4 name=${i}-a k=128 in='${readsfq_path}${i}_1_clean.fq ${readsfq_path}${i}_2_clean.fq'
+mkdir abyss
+cd abyss
+abyss-pe name=${i}-a k=96 in='${readsfq_path}${i}_1_clean.fq ${readsfq_path}${i}_2_clean.fq'
 cp ${i}-a-8.fa ../${i}_a_scaffolds.fa
 cp ${i}-a-stats.tab ../${i}_a_stats.txt
 cd ..
@@ -22,5 +22,5 @@ cd ..
 " > abyss_${i}.sh
 done
 for j in $(cat list); do
-bsub < abyss_${j}.sh
+bsub bash abyss_${j}.sh
 done
